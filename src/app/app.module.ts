712c47500166c272c452e529f0133ecd4b4eba9b
orangetoad <thebiktop@gmail.com>
2017-03-19 00:47:53 +0200

    implemented exercises(added components, service, model)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ExercisesModule } from './exercises/exercises.module';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ExercisesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
